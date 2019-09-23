4aa7aa402ff9a63d2e93fcf82af7bbeb2f403b00
Robert Lee <bobbydwise1@hotmail.com>
2019-09-23 16:03:27 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Daily Coding Problem 2019-09-23
+
+####  _Good morning! Here's your coding interview problem for today._
+
+This problem was asked by Google.
+
+Given the head of a singly linked list, reverse it in-place.
---
+++ scripts.js
@@ -0,0 +1,17 @@
+/*
+This problem was asked by Google.
+
+Given the head of a singly linked list, reverse it in-place.
+*/
+
+
+
+$(document).ready(function() {
+
+  $('#form-1').submit(function(){
+    event.preventDefault()
+    
+    $('#output-1').text(1)
+  })
+
+});
